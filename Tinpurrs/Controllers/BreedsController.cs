1a1241b240c04f0df5f6dfae9b6bf578118f5718
potato707 <mmmodyyasser@gmail.com>
2025-10-09 02:14:42 +0530

    Initial commit

---
+++ Tinpurrs/Controllers/BreedsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tinpurrs.Models;
+
+namespace Tinpurrs.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BreedsController: ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetBreeds([FromQuery] int page, [FromQuery] int limit)
+        {
+            using var client = new HttpClient();
+            string url = $"https://api.thecatapi.com/v1/breeds?limit={limit}&page={page}";
+            var response = await client.GetStringAsync(url);
+            var breeds = System.Text.Json.JsonSerializer.Deserialize<List<CatBreed>>(response);
+            var result = breeds.Select(b => new
+            {
+                name = b.Name,
+                temperament = b.Temperament,
+                description = b.Description,
+                origin = b.Origin,
+                image_url = $"https://cdn2.thecatapi.com/images/{b.ReferenceImageId}.jpg"
+            });
+
+            return Ok(result);
+
+        }
+    }
+}
---
+++ Tinpurrs/Models/CatBreed.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Tinpurrs.Models
+{
+    public class CatBreed
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("temperament")]
+        public string Temperament { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("origin")]
+        public string Origin { get; set; }
+
+        [JsonPropertyName("reference_image_id")]
+        public string ReferenceImageId { get; set; }
+    }
+}
